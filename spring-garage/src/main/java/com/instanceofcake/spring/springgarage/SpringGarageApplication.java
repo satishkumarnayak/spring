37fb4e5fd449fd07b554e358ccd3cb9d777750cc
Satish Nayak <saishkumarNayak@gmail.com>
2019-05-28 20:28:39 +0530

    spring initial setup

---
+++ spring-garage/src/main/java/com/instanceofcake/spring/springgarage/SpringGarageApplication.java
@@ -0,0 +1,14 @@
+package com.instanceofcake.spring.springgarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGarageApplication.class, args);
+		
+	}
+
+}
